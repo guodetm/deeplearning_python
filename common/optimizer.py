25dcfb88e19fb33071b94c6b53b9d4c3fa1dd101
xiao <763485391@qq.com>
2020-05-23 11:36:42 +0800

    add Nesterov class

--- common/optimizer.py
+++ common/optimizer.py
@@ -42,6 +42,27 @@ class AdaGrad:
             grads[key] -= (self.lr * grads[key]) / (np.sqrt(self.h[key]) + 1e-7)
 
 
+class Nesterov:
+    """Nesterov's Accelerated Gradient (http://arxiv.org/abs/1212.0901)"""
+
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+
+    def update(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.v[key] *= self.momentum
+            self.v[key] -= self.lr * grads[key]
+            params[key] += self.momentum * self.momentum * self.v[key]
+            params[key] -= (1 + self.momentum) * self.lr * grads[key]
+
+
 class RMSprop:
     """RMSprop"""
 
