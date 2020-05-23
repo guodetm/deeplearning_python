f14fbb6fa7aef697e63c679ad07fdea38efb293d
xiao <763485391@qq.com>
2020-05-23 11:34:33 +0800

    add RMSprop class

--- common/optimizer.py
+++ common/optimizer.py
@@ -42,6 +42,26 @@ class AdaGrad:
             grads[key] -= (self.lr * grads[key]) / (np.sqrt(self.h[key]) + 1e-7)
 
 
+class RMSprop:
+    """RMSprop"""
+
+    def __init__(self, lr=0.01, decay_rate=0.99):
+        self.lr = lr
+        self.decay_rate = decay_rate
+        self.h = None
+
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.h[key] *= self.decay_rate
+            self.h[key] += (1 - self.decay_rate) * grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+
+
 class Adam:
     """Adam (http://arxiv.org/abs/1412.6980v8)"""
 
