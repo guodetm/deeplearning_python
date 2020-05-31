9c62d78350063d2526e7a661051fa2525ddad6a5
xiao <763485391@qq.com>
2020-05-31 11:28:01 +0800

    create a nomal nn

---
+++ mooc/ch02/two_nn.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 读取数据
+df = pd.read_csv('dot.csv')
+x_data = np.array(df[['x1', 'x2']])
+y_data = np.array(df['y_c'])
+
+# 将数据转换格式
+x_train = np.vstack(x_data).reshape(-1, 2)
+y_train = np.vstack(y_data).reshape(-1, 1)
+Y_c = [['red' if y else 'blue'] for y in y_train]
+
+# 转换数据类型
+x_train = tf.cast(x_train, tf.float32)
+y_train = tf.cast(y_train, tf.float32)
+# from_tensor_slices函数切分传入的张量的第一个维度，生成相应的数据集，使输入特征和标签值一一对应
+data_batch = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(32)
+# 生成神经网络的参数，输入层为2个神经元，隐藏层为11个神经元，1层隐藏层，输出层为1个神经元
+# 用tf.Variable()保证参数可训练
+w1 = tf.Variable(tf.random.normal([2, 11], dtype=tf.float32))
+b1 = tf.Variable(tf.constant([0.01], dtype=tf.float32))
+
+w2 = tf.Variable(tf.random.normal([11, 1], dtype=tf.float32))
+b2 = tf.Variable(tf.constant([0.01], dtype=tf.float32))
+
+lr = 0.005  # 学习率
+epoch = 800
+# 训练部分
+for i in range(epoch):
+    for step, (x_train_step, y_train_step) in enumerate(data_batch):
+        with tf.GradientTape() as tape:
+            h1 = tf.matmul(x_train_step, w1) + b1
+            h1 = tf.nn.relu(h1)
+            y = tf.matmul(h1, w2) + b2
+
+            loss = tf.reduce_mean(tf.square(y_train_step - y))
+        varients = [w1, b1, w2, b2]
+        gradient = tape.gradient(loss, varients)
+        # 更新参数
+        w1.assign_sub(lr * gradient[0])
+        b1.assign_sub(lr * gradient[1])
+        w2.assign_sub(lr * gradient[2])
+        b2.assign_sub(lr * gradient[3])
+    if i % 20 == 0:
+        print('epoch', i, 'loss', float(loss))
+
+# 预测部分
+print("*******predict*******")
+# xx在-3到3之间以步长为0.01，yy在-3到3之间以步长0.01,生成间隔数值点
+xx, yy = np.mgrid[-3:3:.1, -3:3:.1]
+# 将xx , yy拉直，并合并配对为二维张量，生成二维坐标点
+grid = np.c_[xx.ravel(), yy.ravel()]
+grid = tf.cast(grid, tf.float32)
+# 将网格坐标点喂入神经网络，进行预测，probs为输出
+probs = []
+for x_test in grid:
+    # 使用训练好的参数进行预测
+    h1 = tf.matmul([x_test], w1) + b1
+    h1 = tf.nn.relu(h1)
+    y = tf.matmul(h1, w2) + b2  # y为预测结果
+    probs.append(y)
+
+# 取第0列给x1，取第1列给x2
+x1 = x_train[:, 0]
+x2 = x_train[:, 1]
+# probs的shape调整成xx的样子
+probs = np.array(probs).reshape(xx.shape)
+plt.scatter(x1, x2, color=np.squeeze(Y_c))  # squeeze去掉纬度是1的纬度,相当于去掉[['red'],[''blue]],内层括号变为['red','blue']
+# 把坐标xx yy和对应的值probs放入contour函数，给probs值为0.5的所有点上色  plt.show()后 显示的是红蓝点的分界线
+plt.contour(xx, yy, probs, levels=[.5])
+plt.show()
+
+# 读入红蓝点，画出分割线，不包含正则化
+# 不清楚的数据，建议print出来查看
--- tensorflow_test/test.py
+++ tensorflow_test/test.py
@@ -1,10 +1,7 @@
 import tensorflow as tf
+import numpy as np
 tf.compat.v1.disable_eager_execution()
 
 
-a = tf.constant([1.0,2.0],name="a")
-b = tf.constant([3.0,4.0],name="b")
-
-result = a+b
-sess = tf.compat.v1.Session()
-print(sess.run(result))
+a = tf.constant([2,11],dtype=tf.int32)
+print(a)
